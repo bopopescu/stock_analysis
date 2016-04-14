1b9ea6da521f91058694678c1fed10b113729729
Gabriel Preti Santiago <gsantiago@uolinc.com>
2016-04-13 22:12:24 -0300

    Configuracao do diretorio de templates e static

--- stock_analysis_fe/fe_mgmt.sh
+++ stock_analysis_fe/fe_mgmt.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 APP_CONTAINER_NAME="stock_analysis_fe"
+WORKING_DIR=$(pwd)
 
 function build_image {
   docker build -t $APP_CONTAINER_NAME --build-arg app_jar="target/stock_analysis_fe-1.0-SNAPSHOT.jar" .
@@ -7,7 +8,7 @@ function build_image {
 
 function run_container {
   docker rm $APP_CONTAINER_NAME
-  docker run --name $APP_CONTAINER_NAME  -p 8081:8081 -p 8001:8001  --link stock_analysis_api stock_analysis_fe
+  docker run --name $APP_CONTAINER_NAME  -p 8081:8081 -p 8001:8001 -v ${WORKING_DIR}/src/main/resources/:/var/stock_analysis/htdocs/  --link stock_analysis_api stock_analysis_fe
 }
 
 function parse_command {
