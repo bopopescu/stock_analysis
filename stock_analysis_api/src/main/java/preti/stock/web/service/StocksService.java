5b8a56b67eacd87f4f2b5960c25e0c171c028ca9
Gabriel Preti Santiago <gsantiago@uolinc.com>
2016-06-07 16:51:16 -0300

    Correcao no procedimento de carregamento do arquivo da Bovespa: verificacao da existencia de um historico

--- stock_analysis_api/src/main/java/preti/stock/web/service/StocksService.java
+++ stock_analysis_api/src/main/java/preti/stock/web/service/StocksService.java
@@ -22,70 +22,76 @@ import preti.stock.web.repository.StocksRepository;
 
 @Service
 public class StocksService {
-	private Logger logger = LoggerFactory.getLogger(StocksService.class);
+    private Logger logger = LoggerFactory.getLogger(StocksService.class);
 
-	@Autowired
-	private StocksRepository stocksRepository;
+    @Autowired
+    private StocksRepository stocksRepository;
 
-	public void setStocksRepository(StocksRepository stocksRepository) {
-		this.stocksRepository = stocksRepository;
-	}
-	
-	public Stock getStock(long stockId) {
-		return stocksRepository.getStock(stockId);
-	}
+    public void setStocksRepository(StocksRepository stocksRepository) {
+        this.stocksRepository = stocksRepository;
+    }
 
-	public List<Stock> loadStocks(List<String> stockCodes, Date initialDate, Date finalDate) {
-		logger.info("Loading stocks " + stockCodes);
+    public Stock getStock(long stockId) {
+        return stocksRepository.getStock(stockId);
+    }
 
-		List<Stock> stocks = new ArrayList<>();
-		for (String stockCode : stockCodes) {
-			logger.debug("Parsing " + stockCode);
+    public List<Stock> loadStocks(List<String> stockCodes, Date initialDate, Date finalDate) {
+        logger.info("Loading stocks " + stockCodes);
 
-			Stock s = stocksRepository.getStock(stockCode);
-			s.setHistory(stocksRepository.getStockHistory(stockCode, initialDate, finalDate));
-			stocks.add(s);
-		}
+        List<Stock> stocks = new ArrayList<>();
+        for (String stockCode : stockCodes) {
+            logger.debug("Parsing " + stockCode);
 
-		return stocks;
-	}
-	
-	public List<StockHistory> getStockHistory(long stockId, Date initialDate, Date finalDate) {
-		return stocksRepository.getStockHistory(stockId, initialDate, finalDate);
-	}
+            Stock s = stocksRepository.getStock(stockCode);
+            s.setHistory(stocksRepository.getStockHistory(stockCode, initialDate, finalDate));
+            stocks.add(s);
+        }
 
-	@Transactional(propagation = Propagation.REQUIRES_NEW)
-	public void loadStockData(String stockFile) throws IOException {
-		logger.info(String.format("Loading stocks data from file %s ...", stockFile));
+        return stocks;
+    }
 
-		Date lastStockDate = stocksRepository.getLastStocksDate();
-		String.format("Last stock date is %s", lastStockDate);
+    public List<StockHistory> getStockHistory(long stockId, Date initialDate, Date finalDate) {
+        return stocksRepository.getStockHistory(stockId, initialDate, finalDate);
+    }
 
-		// FIXME: verificar se de fato não há leak aqui.
-		@SuppressWarnings("resource")
-		Stream<String> lines = Files.lines(Paths.get(stockFile));
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void loadStockData(String stockFile) throws IOException {
+        logger.info(String.format("Loading stocks data from file %s ...", stockFile));
 
-		Stream<InputDataEntry> inputData;
-		if (lastStockDate != null) {
-			inputData = lines.filter(s -> !s.trim().isEmpty()).map(InputDataEntry::parseFromLine)
-					.filter(entry -> entry.getDate().after(lastStockDate));
-		} else {
-			inputData = lines.filter(s -> !s.trim().isEmpty()).map(InputDataEntry::parseFromLine);
-		}
+        Date lastStockDate = stocksRepository.getLastStocksDate();
+        String.format("Last stock date is %s", lastStockDate);
 
-		inputData.forEach(entry -> {
+        // FIXME: verificar se de fato não há leak aqui.
+        @SuppressWarnings("resource")
+        Stream<String> lines = Files.lines(Paths.get(stockFile));
 
-			if (!stocksRepository.existsStock(entry.getCode())) {
-				logger.info(String.format("Creating stock %s ...", entry.getCode()));
-				stocksRepository.createStock(entry.getCode(), entry.getName());
-			}
+        Stream<InputDataEntry> inputData;
+        if (lastStockDate != null) {
+            inputData = lines.filter(s -> !s.trim().isEmpty()).map(InputDataEntry::parseFromLine)
+                    .filter(entry -> entry.getDate().after(lastStockDate));
+        } else {
+            inputData = lines.filter(s -> !s.trim().isEmpty()).map(InputDataEntry::parseFromLine);
+        }
 
-			Integer stockId = stocksRepository.getStockId(entry.getCode());
-			logger.info(String.format("Creating history code=%s date=%s close=%s open=%s volume=%s", entry.getCode(),
-					entry.getDate(), entry.getClose(), entry.getOpen(), entry.getVolume()));
-			stocksRepository.createHistory(stockId, entry.getDate(), entry.getHigh(), entry.getLow(), entry.getClose(),
-					entry.getOpen(), entry.getVolume());
-		});
+        inputData.parallel().forEach(entry -> {
 
-	}
+            if (!stocksRepository.existsStock(entry.getCode())) {
+                logger.info(String.format("Creating stock %s ...", entry.getCode()));
+                stocksRepository.createStock(entry.getCode(), entry.getName());
+            }
+
+            Integer stockId = stocksRepository.getStockId(entry.getCode());
+            if (stocksRepository.existsHistoryAtDate(stockId, entry.getDate())) {
+                logger.info(String.format("History for stock %s at date %s alread exists", entry.getCode(),
+                        entry.getDate()));
+                return;
+            }
+
+            logger.info(String.format("Creating history code=%s date=%s close=%s open=%s volume=%s", entry.getCode(),
+                    entry.getDate(), entry.getClose(), entry.getOpen(), entry.getVolume()));
+            stocksRepository.createHistory(stockId, entry.getDate(), entry.getHigh(), entry.getLow(), entry.getClose(),
+                    entry.getOpen(), entry.getVolume());
+        });
+
+    }
 }
--- stock_analysis_model/src/main/java/preti/stock/InputDataEntry.java
+++ stock_analysis_model/src/main/java/preti/stock/InputDataEntry.java
@@ -11,112 +11,116 @@ import java.util.Date;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @SuppressWarnings("serial")
 public class InputDataEntry implements Serializable {
+    private static Logger logger = LoggerFactory.getLogger(InputDataEntry.class);
 
-	private Date date;
-	private String code;
-	private String type;
-	private String name;
-	private double open;
-	private double high;
-	private double low;
-	private double close;
-	private double volume;
+    private Date date;
+    private String code;
+    private String type;
+    private String name;
+    private double open;
+    private double high;
+    private double low;
+    private double close;
+    private double volume;
 
-	public InputDataEntry(Date date, String code, String type, String name, double open, double high, double low,
-			double close, double volume) {
-		super();
-		this.date = date;
-		this.code = code;
-		this.type = type;
-		this.name = name;
-		this.open = open;
-		this.high = high;
-		this.low = low;
-		this.close = close;
-		this.volume = volume;
-	}
+    public InputDataEntry(Date date, String code, String type, String name, double open, double high, double low,
+            double close, double volume) {
+        super();
+        this.date = date;
+        this.code = code;
+        this.type = type;
+        this.name = name;
+        this.open = open;
+        this.high = high;
+        this.low = low;
+        this.close = close;
+        this.volume = volume;
+    }
 
-	public Date getDate() {
-		return date;
-	}
+    public Date getDate() {
+        return date;
+    }
 
-	public String getCode() {
-		return code;
-	}
+    public String getCode() {
+        return code;
+    }
 
-	public String getType() {
-		return type;
-	}
+    public String getType() {
+        return type;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public double getOpen() {
-		return open;
-	}
+    public double getOpen() {
+        return open;
+    }
 
-	public double getHigh() {
-		return high;
-	}
+    public double getHigh() {
+        return high;
+    }
 
-	public double getLow() {
-		return low;
-	}
+    public double getLow() {
+        return low;
+    }
 
-	public double getClose() {
-		return close;
-	}
+    public double getClose() {
+        return close;
+    }
 
-	public double getVolume() {
-		return volume;
-	}
+    public double getVolume() {
+        return volume;
+    }
 
-	public String toString() {
-		return String.format("date=%s code=%s type=%s name=%s open=%s high=%s low=%s close=%s volume=%s", date, code,
-				type, name, open, high, low, close, volume);
-	}
+    public String toString() {
+        return String.format("date=%s code=%s type=%s name=%s open=%s high=%s low=%s close=%s volume=%s", date, code,
+                type, name, open, high, low, close, volume);
+    }
 
-	private static String INPUT_PATTERN = "\\d\\d(?<strdate>\\d{8})..(?<code>.{12})(?<type>.{3})(?<name>.{12}).{17}(?<open>\\d{13})(?<high>\\d{13})(?<low>\\d{13}).{13}(?<close>\\d{13}).{49}(?<volume>\\d{18})";
-	private static String DATE_PATTERN = "yyyyMMdd";
-	private static Pattern pattern = Pattern.compile(INPUT_PATTERN);
-	
-	public static InputDataEntry parseFromLine(String line) {
-		Matcher m = pattern.matcher(line);
-		m.find();
-		Date date;
-		try {
-			date = new SimpleDateFormat(DATE_PATTERN).parse(m.group("strdate"));
-			String code = m.group("code").trim();
-			String type = m.group("type").trim();
-			String name = m.group("name").trim();
-			double open = NumberFormat.getInstance().parse(m.group("open").trim()).doubleValue()/100;
-			double high = NumberFormat.getInstance().parse(m.group("high").trim()).doubleValue()/100;
-			double low = NumberFormat.getInstance().parse(m.group("low").trim()).doubleValue()/100;
-			double close = NumberFormat.getInstance().parse(m.group("close").trim()).doubleValue()/100;
-			double volume = NumberFormat.getInstance().parse(m.group("volume").trim()).doubleValue()/100;
-			
-			return new InputDataEntry(date, code, type, name, open, high, low, close, volume);
-		} catch (ParseException e) {
-			throw new RuntimeException(e);
-		}
-		
-	}
+    private static String INPUT_PATTERN = "\\d\\d(?<strdate>\\d{8})..(?<code>.{12})(?<type>.{3})(?<name>.{12}).{17}(?<open>\\d{13})(?<high>\\d{13})(?<low>\\d{13}).{13}(?<close>\\d{13}).{49}(?<volume>\\d{18})";
+    private static String DATE_PATTERN = "yyyyMMdd";
+    private static Pattern pattern = Pattern.compile(INPUT_PATTERN);
 
-	public static void main2(String[] args) throws IOException, ParseException {
-		String inputFile = "/tmp/cotacoes2.txt";
-		
+    public static InputDataEntry parseFromLine(String line) {
+        Matcher m = pattern.matcher(line);
+        m.find();
+        Date date;
+        try {
+            logger.debug(String.format("Parsing line %s", line));
+            date = new SimpleDateFormat(DATE_PATTERN).parse(m.group("strdate"));
+            String code = m.group("code").trim();
+            String type = m.group("type").trim();
+            String name = m.group("name").trim();
+            double open = NumberFormat.getInstance().parse(m.group("open").trim()).doubleValue() / 100;
+            double high = NumberFormat.getInstance().parse(m.group("high").trim()).doubleValue() / 100;
+            double low = NumberFormat.getInstance().parse(m.group("low").trim()).doubleValue() / 100;
+            double close = NumberFormat.getInstance().parse(m.group("close").trim()).doubleValue() / 100;
+            double volume = NumberFormat.getInstance().parse(m.group("volume").trim()).doubleValue() / 100;
 
-		BufferedReader reader = new BufferedReader(new FileReader(inputFile));
-		String line;
-		while ((line = reader.readLine()) != null) {
-			if(line.trim().isEmpty())
-				continue;
-			System.out.println(InputDataEntry.parseFromLine(line));
-		}
-		reader.close();
-	}
+            return new InputDataEntry(date, code, type, name, open, high, low, close, volume);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public static void main2(String[] args) throws IOException, ParseException {
+        String inputFile = "/tmp/cotacoes2.txt";
+
+        BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            if (line.trim().isEmpty())
+                continue;
+            System.out.println(InputDataEntry.parseFromLine(line));
+        }
+        reader.close();
+    }
 
 }
